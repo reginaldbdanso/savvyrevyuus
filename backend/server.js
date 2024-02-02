03d833342e9891b5c54ce1c00658100c8c8f4b82
reginaldbdanso <reginaldbdanso@gmail.com>
2024-02-02 17:09:19 +0000

    Updated origin to allow front-end

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express()
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: "https://savvyrevyuus.onrender.com/"}));
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
